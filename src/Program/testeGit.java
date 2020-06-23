0812c5765c6c15eed91df475ecd516c59dad3ae6
Leonardo Matta <leonar.matta@gmail.com>
2020-06-23 18:06:28 +0100

    Adicionado IF

--- src/Program/testeGit.java
+++ src/Program/testeGit.java
@@ -1,11 +1,27 @@
 package Program;
 
+import java.util.Scanner;
+
 public class testeGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Olá mundo!");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("A que horas vc acordou? ");
+		int hora = sc.nextInt();
+		 
+		if (hora < 12) {
+			System.out.println("Bom dia!");
+		}
+		else if (hora < 18) {
+			System.out.println("Boa tarde!");
+		}
+		else {
+			System.out.println(" Boa noite!");
+		}
+		
 		
 	}
 
